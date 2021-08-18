90bffc49b59e88f44b651c5cf783b07836a27a7f
김현우 <r2872@naver.com>
2021-08-18 15:38:36 +0900

    버튼 / 체크박스 / 라디오버튼 체험

--- app/src/main/java/com/example/second/MainActivity.kt
+++ app/src/main/java/com/example/second/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.second
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.second.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var mBinding: ActivityMainBinding? = null
+
+    private val binding get() = mBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    override fun onDestroy() {
+        mBinding = null
+        super.onDestroy()
     }
 }
